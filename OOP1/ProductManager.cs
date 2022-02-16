6b16ad80f37b2605193ee4e46b99f753d6093afe
abdulkadirozyurt <abdulkadir.ozyurt@gmail.com>
2022-02-16 21:53:51 +0300

    return-void kullanımı

--- OOP1/ProductManager.cs
+++ OOP1/ProductManager.cs
@@ -8,26 +8,48 @@ namespace OOP1
 {
     class ProductManager // bu şekilde bir isimlendirme görürsek, ürünle ilgili operasyonlar olduğunu anlayacağız. (ekleme, silme,güncelleme...)
     {
-        public void Add()
+        public void Add(Product product)
         {
-
+            Console.WriteLine(product.ProductName+" eklendi.");
         }
 
-        public void Delete()
+        public void Delete(Product product)
         {
-
+            Console.WriteLine(product.ProductName+" silindi.");
         }
 
-        public void Update()
+        public void Update(Product product)
         {
-
+            Console.WriteLine(product.ProductName + " güncellendi.");
         }
 
 
 
+        /*
+         
+         void anahtar kelimesi: 
+
+            metoda (git; yap , et , ekle , sil, güncelle ...) şeklinde
+            emir kipinde bir şeyler söyleriz ve metod gider onu yapar ve işlemi bitirir.
+            
+            işlemin sonucunda ne olduğu bilgisine ihtiyacımız yoktur. Bu durumlarda kullanırız.
+         
+         
+         
+         
+         */
 
 
 
+        public int Topla(int sayi1,int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+        }
 
     }
 }
--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -6,7 +6,7 @@ namespace OOP1
     {
         static void Main(string[] args)
         {
-            
+
             Product product1 = new Product();
             product1.Id = 1;
             product1.CategoryId = 2; // 2 numara bizim için mobilya kategorisini temsil etsin.
@@ -22,11 +22,33 @@ namespace OOP1
                 Id = 2,
                 CategoryId = 5,
                 ProductName = "Kalem",
-                UnitsInStock=5,
+                UnitsInStock = 5,
                 UnitPrice = 35
-                
+
             };
 
+            // case sensitive
+            //PascalCase   //camelCase
+            ProductManager productManager = new ProductManager(); // "ProductManager" türündeki "productManager" yeni bir referans numarası almış "ProductManager"dır şeklinde okunur. (Heap'te adres sahibi olur yani.)
+
+            productManager.Add(product1);
+            productManager.Update(product2);
+            productManager.Delete(product1);
+
+
+
+
+            int sonuc = productManager.Topla(7, 5);
+            Console.WriteLine(sonuc*2);
+
         }
+
+       
+        // int double bool ---> değer tip
+        // dizi class abstract class interface ---> referans tip
+
+        // kamp 4. gün videosunda adres vs anlatılıyor.
     }
 }
+
+
