7a18e8ca765413a12275a6a4045a5bfa1e0fc427
abdulkadirozyurt <abdulkadir.ozyurt@gmail.com>
2022-03-02 17:42:08 +0300

    Her birinin kendine özel hesaplama şekli olduğu için interface'e geçiyoruz.

---
+++ OOP3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP3                          // interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+}
